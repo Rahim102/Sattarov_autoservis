a1c52f4fd986098faa320738ce46b95237663f7f
KarimovArslan <kariim06@mail.ru>
2025-09-30 13:49:54 +0500

    Добавьте файлы проекта.

---
+++ TextBlock.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sattarov_autoservis
+{
+    /// <summary>
+    /// Логика взаимодействия для TextBlock.xaml
+    /// </summary>
+    public partial class TextBlock : Page
+    {
+        public TextBlock()
+        {
+            InitializeComponent();
+        }
+    }
+}
